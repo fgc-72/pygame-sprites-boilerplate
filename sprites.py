1ed70ed6f7f6873885f8377324075587ff94dd55
fgc-72 <gfelipec72@gmail.com>
2024-10-09 09:13:27 -0500

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -3,7 +3,7 @@ import pygame
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = []
+        self.images = []
         
         for num in range(1, 6):
             img = pygame.image.load(f"assets/explosion/exp{num}.png")
@@ -11,19 +11,19 @@ class Sprite(pygame.sprite.Sprite):
             self.images.append(img)
         
         self.index = 0
-        self.current_image = self.images[self.index]
-        self.rect = self.current_image.get_rect()
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.counter = 0
     def update(self):
-        animation_speed = 4
+        animation_speed = 3
         
         self.counter += 1
         
-        if self.counter >= animation_speed and self.index > len(self.images) - 1:
+        if self.counter >= animation_speed and self.index < len(self.images) - 1:
             self.counter = 0
             self.index += 1
-            self.current_image = self.images[self.index]
+            self.image = self.images[self.index]
             
         if self.index >= len (self.images) - 1 and self.counter >= animation_speed:
             self.kill()
